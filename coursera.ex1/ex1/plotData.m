5ce96827897ff2c43127594610b215176e1a318a
gets-mukul <mukulpraj9@gmail.com>
2019-01-12 14:29:18 +0530

    excercise 3 done

--- coursera.ex1/ex1/plotData.m
+++ coursera.ex1/ex1/plotData.m
@@ -5,6 +5,7 @@ function plotData(x, y)
 
 figure; % open a new figure window
 plot(x,y,'rx', 'MarkerSize',10);
+axis([4 24 -5 25]);
 ylabel('Profit in $10,000s');
 xlabel('Population of City in 10,000s');
 % ====================== YOUR CODE HERE ======================
