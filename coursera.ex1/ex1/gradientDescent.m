7d79db660cbe364fb1153462a41182d1aa76dec4
gets-mukul <mukulpraj9@gmail.com>
2019-01-12 14:52:25 +0530

    ex-4-done

--- coursera.ex1/ex1/gradientDescent.m
+++ coursera.ex1/ex1/gradientDescent.m
@@ -8,6 +8,11 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
+  
+  error = (X * theta) - y;
+  temp0 = theta(1) - (alpha / m) * sum(error.* X(:,1));
+  temp1 = theta(2) - ( alpha /m ) * sum(error.* X(:,2));
+  theta = [temp0; temp1];
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
